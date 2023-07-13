567504479c7b43f46af1f522b0d03bc230a2e29f
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-07-13 03:19:00 +0300

    Created radio class

---
+++ lib/model/radio.dart
@@ -0,0 +1,10 @@
+class Radio{
+  final String name;
+  final String streamUrl;
+  final String photoUrl;
+
+  Radio({
+    required this.name,
+    required this.streamUrl,
+    required this.photoUrl});
+}
